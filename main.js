448ecf5abb4327eb545fb66f20317b585d1afe83
DimaBobykin <dibobykin@gmail.com>
2021-04-13 18:01:57 -0400

    Forgot to commit JS File

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function colorWord(text, word) {
-    if (text.includes(word)) {
-        text = text.replace(word, "<span style='color:blue;'>" + word + "</span>");
-    }
-    return text;
-}
-
 document.getElementById("editor").addEventListener("input", function() {
     if (document.getElementById("editor").innerHTML.includes("word")) {
         document.execCommand('styleWithCSS', false, true)
