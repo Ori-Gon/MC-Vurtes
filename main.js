f9de4d822216cbb3fcabcad87ab796cbcdf2a407
DimaBobykin <dibobykin@gmail.com>
2021-04-14 20:17:49 -0400

    Update

--- main.js
+++ main.js
@@ -4,4 +4,23 @@ document.getElementById("editor").addEventListener("input", function() {
         document.execCommand('styleWithCSS', false, true)
         document.execCommand('foreColor', false, "#ffe986")
     });
+    //SynHighlight();
 }, false);
+
+/*function SynHighlight(){
+    var text=document.getElementById("editor").innerText
+
+    if(text.indexOf("My name")>-1){
+        text=text.replace("My name", "<span class='blue'>My name</span>");
+    }
+
+    if(text.indexOf("my age")>-1){
+        text=text.replace("my age", "<span class='blue'>my age</span>");
+    }
+
+    document.getElementById("editor").innerHTML=text;
+}
+
+Visibility() {
+
+};*/
