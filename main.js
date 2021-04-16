1da349e552a6d96e97bfea1c0e7710568de0d108
DimaBobykin <dibobykin@gmail.com>
2021-04-16 18:16:22 -0400

    Added Imporing of files

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
 document.getElementById("editor").addEventListener("input", function () {
-    console.log(document.getElementById('editor').innerText)
-    /*document.getElementById('editor').innerText.replace(/\n/gi, function (x) {
-        document.execCommand('styleWithCSS', false, true)
-        document.execCommand('foreColor', false, "#ffe986")
-    });*/
-    //SynHighlight();
 }, false);
 
-/*function SynHighlight(){
-    var text=document.getElementById("editor").innerText
-
-    if(text.indexOf("My name")>-1){
-        text=text.replace("My name", "<span class='blue'>My name</span>");
-    }
-
-    if(text.indexOf("my age")>-1){
-        text=text.replace("my age", "<span class='blue'>my age</span>");
-    }
-
-    document.getElementById("editor").innerHTML=text;
-}
-
-Visibility() {
-
-};*/
-
 function FileChange() {
-    console.log(document.getElementById("Upload").innerText);
+    var reader = new FileReader();
+    reader.readAsText(document.getElementById("Upload-Files").files[0], "UTF-8");
+    reader.onload = function (evt) {
+        document.getElementById("editor").innerText = evt.target.result;
+    }
+    reader.onerror = function (evt) {
+        document.getElementById("editor").innerText = "error reading file";
+    }
 };
 
 function PopupDisplay(IdOfPopup) {
